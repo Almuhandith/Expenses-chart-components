bfbd47213de80d2b69da3710a1ea207f6c6b124a
KasiePatricia <ugwukasiep@gmail.com>
2023-03-15 10:09:31 +0100

    Added some colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Logo from "../../images/logo.svg";
+import Logo from "../images/logo.svg";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center bg-[#ec775f] p-5 w-[27.5rem] rounded-xl">
+    <div className="flex justify-between items-center bg-SoftRed p-5 w-[21.5rem] rounded-xl">
       <div className="text-white">
-        <h1 className="text-lg font-normal">My Balance</h1>
+        <h1 className="text-lg font-normal text-Cream">My balance</h1>
         <p className="text-2xl font-bold">$921.48</p>
       </div>
       <div>
